1c0f5fc4fb98a38cf2e3eba97191334939cd2a1c
tamionv <tamionv@gmail.com>
2019-10-23 01:48:23 +0100

    Improved `Parser.hs`
    The parser now accepts and generates lambda terms that are not "fully
    parenthesised"

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,12 +7,18 @@ import Data.Char
 
 instance Show Term where
     show term = case term of
-        App s t -> "(" ++ show s ++ show t ++ ")"
-        Abs x t -> "(" ++ "\\" ++ x ++ "." ++ show t ++ ")"
-        Var x -> "(" ++ x ++ ")"
+        App (Var x) (Var y) -> x ++ " " ++ y
+        App (Var x) y -> x ++ "(" ++ show y ++ ")"
+        App (abs@(Abs _ _)) (Var y) -> "(" ++ show abs ++ ")" ++ y
+        App (abs@(Abs _ _)) y -> "(" ++ show abs ++ ")" ++ "(" ++ show y ++ ")"
+        App (app@(App _ (Var _))) (Var z) -> show app ++ " " ++ z
+        App (app@(App _ _)) (Var z) -> show app ++ z
+        App (app@(App _ _)) z -> show app ++ "(" ++ show z ++ ")"
+        Abs x t -> "\\" ++ x ++ "." ++ show t
+        Var x -> x
 
 ident :: ReadP Ident
-ident = munch isAlpha
+ident = munch1 isAlpha
 
 parens :: ReadP a -> ReadP a
 parens x = do
@@ -23,16 +29,39 @@ parens x = do
     char ')'
     return r
 
-app :: ReadP Term
-app = liftM2 App term term
+var :: ReadP Term
+var = skipSpaces >> Var <$> ident
+
+appRhs :: ReadP Term
+appRhs = var +++ parens Parser.abs +++ parens app
+
+varLhsApp :: ReadP Term
+varLhsApp = liftM2 App var appRhs
+
+absLhsApp :: ReadP Term
+absLhsApp = liftM2 App (parens Parser.abs) appRhs
+
+remainingApp :: Term -> ReadP Term
+remainingApp current = liftM (App current) $ (parens term +++ var)
+
+appLhsApp :: ReadP Term
+appLhsApp = (varLhsApp +++ absLhsApp +++ parens app) >>= remainingApp
 
 abs :: ReadP Term
-abs = liftM2 Abs (skipSpaces >> char '\\' >> ident) (skipSpaces >> char '.' >> term)
+abs = do
+    skipSpaces
+    char '\\'
+    x <- ident
+    skipSpaces
+    char '.'
+    t <- term
+    return $ Abs x t
 
-var :: ReadP Term
-var = Var <$> ident
-
-term = (parens app +++ parens Parser.abs +++ parens var)
+app :: ReadP Term
+app = varLhsApp +++ appLhsApp +++ absLhsApp
+ 
+term :: ReadP Term
+term = var +++ app +++Parser.abs
 
 instance Read Term where
     readsPrec _ = readP_to_S term
